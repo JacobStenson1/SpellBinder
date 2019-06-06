ff685cf0b710ccc362c371a1ef85236a84b52e86
Jacob Stenson <jacob@nosnets.co.uk>
2019-06-06 21:46:43 +0100

    Initial Commit
    Working on creating a shooting animation

---
+++ 2DSpellBinder/Assets/PlayerActions.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerActions : MonoBehaviour
+{
+
+    public GameObject fireball;
+    public Transform firePoint;
+
+    public Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("e"))
+        {
+            print("Testing");
+            animator.Play("Shoot");
+        }
+    }
+
+    void Shoot()
+    {
+        Instantiate(fireball, firePoint.position, firePoint.rotation);
+    }
+}
